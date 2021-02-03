0660fc46bc3b28947fc760681302e1047ac32310
Michael Henry <michaelkenyi13@gmail.com>
2021-02-03 19:43:29 +0600

    resume build with react js

---
+++ src/Components/Resume.js
@@ -0,0 +1,90 @@
+import React from "react";
+import AOS from 'aos';
+
+const Resume = ({ data }) => {
+  if (data) {
+    var skillmessage = data.skillmessage;
+    AOS.init({
+      duration : 2000
+    })
+    var education = data.education.map(function (education) {
+      return (
+        <div key={education.school} data-aos="fade-up" data-aos-delay="300">
+          <h3>{education.school}</h3>
+          <p className="info">
+            {education.degree} <span>&bull;</span>
+            <em className="date">{education.graduated}</em>
+          </p>
+          <p>{education.description}</p>
+        </div>
+      );
+    });
+    var work = data.work.map(function (work) {
+      return (
+        <div key={work.company}  data-aos="fade-up" data-aos-delay="600">
+          <h3>{work.company}</h3>
+          <p className="info">
+            {work.title}
+            <span>&bull;</span> <em className="date">{work.years}</em>
+          </p>
+          <p>{work.description}</p>
+        </div>
+      );
+    });
+    var skills = data.skills.map(function (skills) {
+      var className = "bar-expand " + skills.name.toLowerCase();
+      return (
+        <li key={skills.name} data-aos="fade-right" data-aos-delay="600">
+          <span style={{ width: skills.level }} className={className}></span>
+          <em>{skills.name}</em>
+        </li>
+      );
+    });
+  }
+
+  return (
+    <section id="resume">
+      <div className="row education">
+        <div className="three columns header-col">
+          <h1>
+            <span data-aos="fade-up" data-aos-delay="300">Education</span>
+          </h1>
+        </div>
+
+        <div className="nine columns main-col">
+          <div className="row item">
+            <div className="twelve columns">{education}</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row work">
+        <div className="three columns header-col">
+          <h1>
+            <span data-aos="fade-up" data-aos-delay="600">Work</span>
+          </h1>
+        </div>
+
+        <div className="nine columns main-col">{work}</div>
+      </div>
+
+      <div className="row skill">
+        <div className="three columns header-col">
+          <h1>
+            <span data-aos="fade-left" data-aos-delay="300">Skills</span>
+          </h1>
+        </div>
+
+        <div className="nine columns main-col">
+          <p>{skillmessage}</p>
+
+          <div className="bars">
+            <ul className="skills">{skills}</ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Resume;
